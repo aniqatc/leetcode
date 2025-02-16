f1d793461857f4141e288f999558a3c515655b30
aniqatc <aniqatc97@gmail.com>
2025-02-16 14:06:03 -0500

    0001: add test cases

--- 0001-two-sum/solution.js
+++ 0001-two-sum/solution.js
@@ -23,7 +23,18 @@ function twoSum(nums, target) {
 		if (values.has(difference)) {
 			return [values.get(difference), n];
 		}
-
 		values.set(nums[n], n);
 	}
+	return [];
 }
+
+// Manual Tests
+console.log(twoSum([1, 2, 3, 4, 5], 9)); // [3, 4]
+console.log(twoSum([0, 0, 1, 1], 1)); // [1, 2]
+console.log(twoSum([7, 10, 3, 2, 6, 0, 9, 11], 21)); // [1, 7]
+console.log(twoSum([-3, -3], -6)); // [0, 1]
+
+// Edge Cases
+console.log(twoSum([0, 9, 8], 10)); // []
+console.log(twoSum([1], 10)); // []
+console.log(twoSum([], 10)); // []
